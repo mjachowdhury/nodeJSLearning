2b88ab5972d002463da1e71cbc156c466c4a5eb7
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-05 17:22:52 +0100

    added setInterval function example

--- timer.js
+++ timer.js
@@ -1,7 +1,18 @@
 const waitTime = 3000;
+const waitInterval = 500; //mili sec
+let currentTimne  = 0;
+
+const inncrementTime = () => {
+    currentTimne += waitInterval;
+    console.log(`Waiting ${ currentTimne / 1000 } seconds`);
+};
+
 console.log(`setting a ${ waitTime / 1000} second delay`);
 
-const timerFinished = () => console.log("Done");
-
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log("Done");
+};
+const interval = setInterval(inncrementTime, waitInterval);
 //setTimeout function
 setTimeout(timerFinished, waitTime);
