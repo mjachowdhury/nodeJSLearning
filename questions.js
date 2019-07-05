132d71155b221aa7a549f559a86f2045736a6e21
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-05 16:12:34 +0100

    Used event listner during exit the program.

--- questions.js
+++ questions.js
@@ -12,7 +12,27 @@ const ask = ( i = 0 ) =>{
 ask();
 
 //with on functon we can get user input and it wil not stop the progeam. but when u put process.exit() then it will exit the program.
+
+const answers = [];
 process.stdin.on("data", data => {
-    process.stdout.write(`\n\n ${ data.toString().trim() } \n\n`); 
-    process.exit();
+    answers.push(data.toString().trim()); // here pusing to the pusing function and adding the user answer to the answers array
+    //process.stdout.write(`\n\n ${ data.toString().trim() } \n\n`); 
+    if(answers.length < questions.length) { //here checking the length of hte question as wel las answer
+        ask(answers.length);
+    }else{
+        process.exit();
+    }
+    
 });
+
+
+// exit listner the programm nicelly
+ process.on("exit", () => {
+    const [name, activity, language] = answers;
+    console.log(`
+    
+    Thank you for your answer.
+    Go ${ activity }  ${ name  } you can write ${ language } code later!!!
+    
+    `);
+ });
