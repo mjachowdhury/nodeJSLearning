a553a29a319457c1ac40f7a0d1a5d749b52cc7da
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-05 17:29:22 +0100

    Example of percentage showing during programm running

--- timer.js
+++ timer.js
@@ -1,10 +1,14 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitInterval = 500; //mili sec
 let currentTimne  = 0;
 
 const inncrementTime = () => {
     currentTimne += waitInterval;
-    console.log(`Waiting ${ currentTimne / 1000 } seconds`);
+    const percentage = Math.floor(( currentTimne / waitTime ) * 100 );
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0); //will take the cursor to the first possition
+    process.stdout.write(` waiting ... ${ percentage }%`);
+    //console.log(`Waiting ${ currentTimne / 1000 } seconds`);
 };
 
 console.log(`setting a ${ waitTime / 1000} second delay`);
