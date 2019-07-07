bdada3439f220e3fd569162950dc22d7143be8b5
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-07 19:19:40 +0100

    Added lib folder and inside that folder that function usr can use it with any name as it is a module function and that module is used inside the questionAsk.js file.

--- questionAsk.js
+++ questionAsk.js
@@ -1,37 +1,12 @@
-const readline = require("readline"); //readline module
-
-//creating interface
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
+ const collectAnswers = require("./lib/collectAnswer");
 //array of questions
 const questions =[
     "What is your name?",
     "Where do you live?",
     "Do you like NodeJs?"
 ];
-
-//collect answer function
-const collectAnswer = (questions, done) =>{
-    const answers = [];
-
-    const[firstQuestion] = questions;
-
-    const questionAnswered = answer => {
-        answers.push(answer);//pushing answer to the answers array
-        //condition to check if all the question is asked or not...
-        if(answers.length < questions.length) {
-            rl.question(questions[answers.length], questionAnswered);
-        }else{
-            done(answers);
-        }
-    }
-
-    rl.question(firstQuestion, questionAnswered);
-};
-collectAnswer( questions, answers =>{
+ 
+collectAnswers( questions, answers =>{
     console.log("Thank you for your answaer.");
     console.log(answers);
     process.exit();
