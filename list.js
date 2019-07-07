ffa9e61e6668bef2cf1ab138511ccadd1eec28c1
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-07 22:22:04 +0100

    files, dir reading module fs example with sys and asyn..

--- list.js
+++ list.js
@@ -1,5 +1,15 @@
 const fs = require("fs");
 console.log('Start reading file....');
-const files = fs.readdirSync("./lib"); //this will do syncronizely 
-console.log('Complete reading files. ');
-console.log(files);
+const files = fs.readdirSync("./lib"); //this will do syncronizely that means javascript will stop doing until it finish reading all the content that directory
+
+
+//this way it will read asyncronizly... where javaScript will not wait to finish the reading contents
+fs.readdir("./lib", (err, files) => {
+    if(err) {
+        throw err;
+    }
+
+    console.log('Complete reading files. ');
+    console.log(files);
+
+});
