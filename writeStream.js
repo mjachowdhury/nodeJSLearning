21500536c7b332fe2fb3da49a3f32ec8b34cb515
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-08 11:27:21 +0100

    example of how to writeStream into the file.

--- writeStream.js
+++ writeStream.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
 
 const writeStream = fs.createWriteStream('./storage-files/myTxt.txt', 'UTF-8');
+const readStream = fs.createReadStream('./lib/Readme.md', 'UTF-8');
 
-writeStream.write(`As salamu alykum. \n`);
-writeStream.write('How are you');
+//with this it will write from lib dir file ReadMe.md file to storage-files myTxt.txt
+readStream.pipe(writeStream);
+
+//with this method it will write whatever we type on the terminal
+// process.stdin.on('data' , data => {
+//     writeStream.write(data);
+// });
+
+//this will write to the file
+//writeStream.write(`As salamu alykum. \n`);
+//writeStream.write('How are you');
